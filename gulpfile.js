2d030efcf854ad129fffd586fdca13629cb5c402
pirhoo <pierre.romera@gmail.com>
2014-09-07 16:45:06 +0200

    Removed images compression

--- gulpfile.js
+++ gulpfile.js
@@ -45,11 +45,6 @@ gulp.task('html', ['styles', 'scripts'], function () {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
-        })))
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
